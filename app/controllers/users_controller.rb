b5ab227f3f309c3e8ebf259a5aeb34c98fb125dd
Nikita Gimon <nikitagimon@gmail.com>
2018-07-07 16:36:22 +0200

    Fix alert UI and add route after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "User was successfully created"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
