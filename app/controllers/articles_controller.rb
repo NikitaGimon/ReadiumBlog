09bf5b58cfc8678e904d56caadb1ce28c636ee18
Nikita Gimon <nikitagimon@gmail.com>
2018-07-07 13:25:31 +0200

    Fix layouts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
 
     def index
-        @articles = Article.paginate(page: params[:page], per_page: 1)
+        @articles = Article.paginate(page: params[:page], per_page: 4)
     end
     
     def new
@@ -12,7 +12,8 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.find(2)
+        # @article.user = User.find(2)
+        @article.user = current_user
         if @article.save
             flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
@@ -35,7 +36,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-       
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
 
     def index
-        @users = User.paginate(page: params[:page], per_page: 1)
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
