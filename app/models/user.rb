7751cc042c941d0182237101644f79d432f29750
Nikita Gimon <nikitagimon@gmail.com>
2018-07-08 09:10:39 +0200

    Add admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :articles
+    has_many :articles, dependent: :destroy
     before_save { self.email = email.downcase }
     validates :username, presence: true, length: { minimum: 3, maximum: 50}, 
                          uniqueness: {case_sensitive: false} #ignore cases, doesn't allows adding Joe joe
