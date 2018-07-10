3c465b90151c25c05b53299467c87e9b7d2a6b8d
Nikita Gimon <nikitagimon@gmail.com>
2018-07-10 18:31:39 +0200

    Add category edit button and restrictions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,8 @@ class CategoriesController < ApplicationController
     end
 
     def show
+        @category = Category.find(params[:id])
+        @category_articles = @category.articles.all
     end
 
     def new
@@ -19,6 +21,20 @@ class CategoriesController < ApplicationController
             redirect_to categories_path
         else
             render 'new'
+        end
+    end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Category name was successfully updated"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
         end
     end
 
